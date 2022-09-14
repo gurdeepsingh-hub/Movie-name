ea61f23928164e81109e86cb59cd487bdbe82522
guri <khalsagudeep@gmail.com>
2022-09-14 22:21:22 +0530

    feat- add movie posters and main part of page

--- loops.js
+++ loops.js
@@ -6,7 +6,7 @@ function MenuPages(){
     });
 }
 
-let navlist = ['action.jfif','Detective.jfif','Fantasy.jfif','Space.jpg', 'Blockbuster.jpg'];
+let navlist = ['Action.jfif','Detective.jfif','Fantasy.jfif','Space.jpg', 'Blockbuster.jpg'];
 navListItems();
 function navListItems(){
     navlist.forEach((item) => {
@@ -33,3 +33,14 @@ function AddToNavList(){
     navlist.push('new.jpg');
     console.log(navlist);
 }
+let Posterlist = ['1.jpg','2.jpg','3.jfif','4.webp', '5.jfif','6.jfif','7.webp','8.jfif','9.jfif','10.jfif'];
+Posters();
+function Posters(){
+    Posterlist.forEach((item) => {
+        document.getElementById("Posters").innerHTML += `<div class="poster">
+            <img src="./images/MoviePoster/`+item+`" alt="`+item.substring(0,item.lastIndexOf('.'))+` image" class="posterImage" />
+            <p class="MovieName">`+ item.substring(0,item.lastIndexOf('.'))+`</p>
+            <p class="MovieCategory">Fantasy</p>
+          </div>`
+    });
+  }
